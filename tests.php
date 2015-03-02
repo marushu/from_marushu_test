51cf4a237229f6440c06f73841484db534021949
NishimuraShuhei <shuhei@NishimuraShuhei-no-MacBook-Pro.local>
2015-03-03 00:13:52 +0900

    comment out

--- tests.php
+++ tests.php
@@ -12,7 +12,7 @@ Domain Path: /languages
 
 //アップグレード通知の無効化
 // Thanks to http://www.warna.info/archives/781/
-remove_action( 'wp_version_check', 'wp_version_check' );
+//remove_action( 'wp_version_check', 'wp_version_check' );
 remove_action( 'admin_init', '_maybe_update_core' );
 add_filter( 'pre_site_transient_update_core', '__return_zero' );
 
